b4daf3618a61dca4b95dd0a3ee2e22bb0c77ba38
dirtyOldTown <boloks70@gmail.com>
2022-05-28 15:30:18 +0200

    update media query

--- knjiga.js
+++ knjiga.js
@@ -41,52 +41,55 @@ for (let i = 0; i < navBar.length; i++) {
   })
 }
 // Sređivanje druge sekcije (biografija-text)
-const mediaHandler = window.matchMedia("(max-width: 667px)");
+const mediaHandler = window.matchMedia("screen and (max-width: 667px)" );
+
 const biografija = document.querySelector(".biografija-text");
 const slika = document.querySelector(".biografija img");
 
-function promena() {
-if (!mediaHandler.matches) {
+function promena(x) {
+if (x.matches) {
   biografija.innerHTML = `
-  <pre>
-  <span>a</span>
   Do you believe in destiny? Do you think that everything happens for a 
   reason? Do you ever have that feeling of not belonging?
   My book I DO NOT BELONG is a story of a young girl desperately trying to 
   find herself and her place in the world after a tragic event in her life. A 
   chance encounter with an elderly American couple and a misunderstanding 
   on their part changes her life forever.
+  <hr>
   She is catapulted into a new world searching for her destiny. Will she ever 
-    find it? It's a relatable story inspired by true events. An easy and fast read. 
+  find it? It's a relatable story inspired by true events. An easy and fast read. 
   You can find it on Amazon in paperback or kindle. And all the art
   throughout the book, including the cover, is mine.
   Hope you enjoy reading it. Please let me know. On Amazon, tweeter, Insta
   gram or Facebook. Until we meet again! 
-  <span>b</span>
-  </pre>
   `;
-  biografija.setAttribute("style", "font-size: 110%;; padding: 0px; text-align: center; width: 100%");
-  slika.setAttribute("style", "top: 4px");
+  biografija.classList.remove("biografija-text");
+  biografija.classList.add("mobilni");
+  slika.setAttribute("style", "margin-top: 40px");
 } else {
   biografija.innerHTML = `
-    Do you believe in destiny? Do you think that everything happens for a 
-    reason? Do you ever have that feeling of not belonging?
-    My book I DO NOT BELONG is a story of a young girl desperately trying to 
-    find herself and her place in the world after a tragic event in her life. A 
-    chance encounter with an elderly American couple and a misunderstanding 
-    on their part changes her life forever.
-    <hr>
-    She is catapulted into a new world searching for her destiny. Will she ever 
-    find it? It's a relatable story inspired by true events. An easy and fast read. 
-    You can find it on Amazon in paperback or kindle. And all the art
-    throughout the book, including the cover, is mine.
-    Hope you enjoy reading it. Please let me know. On Amazon, tweeter, Insta
-    gram or Facebook. Until we meet again! 
+  <pre>
+      <span>a</span>
+      Do you believe in destiny? Do you think that everything happens for a 
+      reason? Do you ever have that feeling of not belonging?
+      My book I DO NOT BELONG is a story of a young girl desperately trying to 
+      find herself and her place in the world after a tragic event in her life. A 
+      chance encounter with an elderly American couple and a misunderstanding 
+      on their part changes her life forever.
+      She is catapulted into a new world searching for her destiny. Will she ever 
+        find it? It's a relatable story inspired by true events. An easy and fast read. 
+      You can find it on Amazon in paperback or kindle. And all the art
+      throughout the book, including the cover, is mine.
+      Hope you enjoy reading it. Please let me know. On Amazon, tweeter, Insta
+      gram or Facebook. Until we meet again! 
+      <span>b</span>
+    </pre>
 `;
-  biografija.setAttribute("style", "font-size: 1.1em; padding: 12px; text-align: justify; width: 90%");
-  slika.setAttribute("style", "margin-top: 40px");
+biografija.classList.remove("mobilni");
+biografija.classList.add("biografija-text");
+  slika.setAttribute("style", "top: 4px");
 }
 }
-mediaHandler.onchange = () => {
-  promena();
-}
+
+promena(mediaHandler);
+mediaHandler.addListener(promena)
