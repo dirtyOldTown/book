99612e582c1be1a46b1f560a3237212dca7ff2cd
dirtyOldTown <boloks70@gmail.com>
2022-05-31 10:32:56 +0200

    update all

--- knjiga.js
+++ knjiga.js
@@ -41,7 +41,7 @@ for (let i = 0; i < navBar.length; i++) {
   })
 }
 // Sređivanje druge sekcije (biografija-text)
-const mediaHandler = window.matchMedia("screen and (max-width: 667px)" );
+const mediaHandler = window.matchMedia("screen and (max-width: 599px)" );
 
 const biografija = document.querySelector(".biografija-text");
 const slika = document.querySelector(".biografija img");
@@ -69,10 +69,10 @@ if (x.matches) {
 } else {
   biografija.innerHTML = `
   <pre>
-      <span>a</span>
+      <span>&#99;</span>
       Do you believe in destiny? Do you think that everything happens for a 
       reason? Do you ever have that feeling of not belonging?
-      My book I DO NOT BELONG is a story of a young girl desperately trying to 
+      My book <a href="#link">I DO NOT BELONG</a> is a story of a young girl desperately trying to 
       find herself and her place in the world after a tragic event in her life. A 
       chance encounter with an elderly American couple and a misunderstanding 
       on their part changes her life forever.
@@ -82,7 +82,7 @@ if (x.matches) {
       throughout the book, including the cover, is mine.
       Hope you enjoy reading it. Please let me know. On Amazon, tweeter, Insta
       gram or Facebook. Until we meet again! 
-      <span>b</span>
+      <span>&#98;</span>
     </pre>
 `;
 biografija.classList.remove("mobilni");
@@ -96,29 +96,39 @@ mediaHandler.addEventListener("change",promena);
 
 // Promene za mobilni
 const brisanjeSlike = window.matchMedia("(max-width: 500px)");
+
 const četkice = document.getElementById("četkice");
-const artBlok1 = document.getElementById("art");
+const artBlok1 = document.querySelector(".art-blok1");
 const palme = document.querySelector(".palme");
 const palmice = document.querySelector(".palme img");
 const text = document.querySelector(".palme p");
 const aboutMima = document.querySelector(".about");
 const aboutMima2 = document.querySelector(".about2");
-
+const mima = document.querySelector(".about pre");
+const palme2Parent = document.querySelector(".palme2");
+const palme2 = document.querySelector(".palme2 p");
+const palme3Parent = document.querySelector(".palme3");
+const palme3 = document.querySelector(".palme3 p");
 function promena2(x) {
   if (x.matches) {
-    // Brisanje slike "četkice" i bojenje pozadine tamno sivom bojom
+    // Brisanje slike "četkice" i bojenje pozadine tamno sivom bojom i ostalo
     artBlok1.removeChild(četkice);
     artBlok1.style.backgroundColor = "#343a40";
     artBlok1.style.color = "#f2f2f2";
+    artBlok1.style.marginTop = "20px";
+    artBlok1.style.paddingBottom = "20px";
+    mima.style.marginLeft = "-1px";
     palmice.style.marginLeft = "8%";
-    palmice.style.marginTop = "44px";
+    palmice.style.marginTop = "50px";
     text.style.marginLeft = "8%";
+    palme2.parentElement.removeChild(palme2);
+    palme3.parentElement.removeChild(palme3);
     aboutMima.innerHTML = `
     <pre style="font-size: 3.4vw">
-    <q>This is a place for a qoute about paintings moved from former as 
-    a young adult after completing her degree in English and Literature 
-    from the University of Belgrade.</q>
-  </pre>
+  <q>This is a place for a qoute about paintings moved from former as 
+  a young adult after completing her degree in English and Literature 
+  from the University of Belgrade.</q>
+    </pre>
     `
     aboutMima2.innerHTML = `
     <pre style="font-size: 3.4vw">
@@ -130,25 +140,28 @@ function promena2(x) {
   } else {
    artBlok1.insertBefore(četkice, palme);
    artBlok1.style.backgroundColor = "transparent";
+   artBlok1.style.marginTop = "30px"
    artBlok1.style.color = "black";
+   artBlok1.style.paddingBottom = "0px";
+   palme2Parent.append(palme2);
+   palme3Parent.append(palme3);
    aboutMima.innerHTML = `
-   <pre>
-    <q>This is a place for a qoute about paintings moved from former as a young adult
-    after completing her degree in English and Literature from the
-    University of Belgrade.</q>
-  </pre>
+    <pre>
+  <q>This is a place for a qoute about paintings moved from former as a young adult
+  after completing her degree in English and Literature from the
+  University of Belgrade.</q>
+    </pre>
    `
    aboutMima2.innerHTML = `
-   <pre>
-    <q>This is a place for a qoute about paintings moved from former as a young adult
-    after completing her degree in English and Literature from the
-    University of Belgrade.</q>
-  </pre>
+    <pre>
+  <q>This is a place for a qoute about paintings moved from former as a young adult
+  after completing her degree in English and Literature from the
+  University of Belgrade.</q>
+    </pre>
    `
-
-  }
-  // Unošenje blok-elementa iznad slike sa natpisom - Mima's arts
+}
 
 }
+
 promena2(brisanjeSlike);
 brisanjeSlike.addEventListener("change", promena2);
