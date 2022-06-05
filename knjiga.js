7c52a16543640fdc0d65da58a572ee7b927da941
dirtyOldTown <boloks70@gmail.com>
2022-06-05 15:54:42 +0200

    update responsive

--- knjiga.js
+++ knjiga.js
@@ -1,46 +1,46 @@
 // PROMENE ZA LAPTOP + MOBILNI
 //-----------------------------
 
-// Animacija za email i facebook
+// Animacija za email, facebook (Promena boja: mouseover/mouseout)
 
 const email = document.querySelector(".kontakt-imejl p:last-of-type a");
 const facebook = document.querySelector("#facebook a");
-// Funkcije za promenu boje
+// Funkcije za animaciju
 function plavaBoja(x) {
   x.setAttribute("style", "color: #90e0ef")
 }
 function belaBoja(x) {
     x.setAttribute("style", "color: white; background-color: transparent")
   }
-// Promena boje u plavo
+// mouseover (addEventListener)
 email.addEventListener("mouseover", () => {
   plavaBoja(email)
 });
 facebook.addEventListener("mouseover", () => {
   plavaBoja(facebook)
 });
-// Promena boje u belo (vraćanje na startne vrednosti)
+//  mouseout (addEventListener)
 email.addEventListener("mouseout", () => {
   belaBoja(email)
 });
 facebook.addEventListener("mouseout", () => {
   belaBoja(facebook)
 });
+// Animacija za navigation-bar 
 
-// Animacija za navigation 
 const navBar = document.querySelectorAll(".navigation-bar li a");
-
+// Looping: moouseover (addEventListener)
 for (let i = 0; i < navBar.length; i++) {
   navBar[i].addEventListener("mousemove", () => {
    navBar[i].setAttribute("style", "color: yellow")
   })
 }
-
+// Looping: moouseout (addEventListener)
 for (let i = 0; i < navBar.length; i++) {
   navBar[i].addEventListener("mouseout", () => {
    navBar[i].setAttribute("style", "color: white")
   })
 }
 
-// PROMENE ZA MOBILNI
+// MOBILNI
 //---------------------
